5761846a2d46318821f5c2f94eea9e9863c123c0
Alex Ballas <alex@ballas.org>
2023-05-16 18:27:23 +0300

    Amend unit test issue

--- main_test.go
+++ main_test.go
@@ -10,13 +10,13 @@ import (
 
 func TestNewContextReader(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
 
 	reader := NewContextReader(ctx, strings.NewReader("Test"))
-	cancel()
 
 	_, err := io.Copy(io.Discard, reader)
 
 	if !errors.Is(err, context.Canceled) {
-		t.Errorf("Unexpected error. Expected '%s', got '%s'", context.Canceled, err)
+		t.Errorf("Unexpected error. Expected '%s', got '%s'", context.Canceled, err.Error())
 	}
 }
